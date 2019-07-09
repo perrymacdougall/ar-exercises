fab59bcda9e102e402e35c19f43df0d433258437
Perry <perrymacdougall@gmail.com>
2019-07-09 19:02:43 +0000

    killed exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+puts @store1.name
+
+@store2 = Store.find_by(id: 2)
+puts @store2.name
+
+@store1.name = 'Toronto'
+@store1.save
+
+puts @store1.name
