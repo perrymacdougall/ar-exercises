4b8450a0bde864ca8e845610e4b44699d0334b40
Perry <perrymacdougall@gmail.com>
2019-07-09 21:19:18 +0000

    exercise_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue
+
+@avg_revenue = Store.average(:annual_revenue)
+puts @avg_revenue
+
+@profitable_stores = Store.where('annual_revenue > 1000000').size
+puts @profitable_stores
+
+# @all_stores = Store.all
+# @all_stores.each do |store|
+#   puts store.name
+# end
